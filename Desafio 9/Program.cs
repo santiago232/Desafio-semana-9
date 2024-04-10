ca562c85f3b9acbbe69edfddc5d80b19b8201734
fazt <wilson@faztweb.com>
2024-04-10 16:07:35 -0600

    Termiando

---
+++ Desafio 9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class EmailExtractor
+{
+    public static void Main(string[] args)
+    {
+        string inputText = "Para más información, envíe un correo a SuperMario1l10@gmail.com o juan.perez@example.com.";
+        List<string> extractedEmails = ExtractEmails(inputText);
+
+        Console.WriteLine("Direcciones de correo electrónico encontradas:");
+        foreach (string email in extractedEmails)
+        {
+            Console.WriteLine(email);
+        }
+    }
+
+    public static List<string> ExtractEmails(string text)
+    {
+        List<string> emails = new List<string>();
+        string emailPattern = @"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b";
+
+        MatchCollection matches = Regex.Matches(text, emailPattern, RegexOptions.IgnoreCase);
+        foreach (Match match in matches)
+        {
+            emails.Add(match.Value);
+        }
+
+        return emails;
+    }
+}
